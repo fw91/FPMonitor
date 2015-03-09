0fcaaa6e16d8703d5e6e040aafda885da510b3f8
FloW <fw-91@gmx.de>
2015-03-09 19:03:23 +0100

    For Testing

--- app/src/main/java/de/lmu/mvs/fpmonitor/MainActivity.java
+++ app/src/main/java/de/lmu/mvs/fpmonitor/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String test = "Just a Teststring";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
